86e13193171db48f88fda829d76fbbcf1244bb16
entchev <micishi@gmail.com>
2021-02-18 09:55:10 +0000

    update v2.0

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -5,8 +5,16 @@ const BlogContext = React.createContext()
 
 const blogReducer = (state, action) => {
   switch (action.type) {
+    case 'delete_blogPost':
+      return state.filter((blogPost) => blogPost.id !== action.payload)
     case 'add_blogPost':
-      return [...state, { title: `Blog Post ${state.length + 1}` }]
+      return [
+        ...state,
+        {
+          id: Math.floor(Math.random() * 9999),
+          title: `Blog Post ${state.length + 1}`,
+        },
+      ]
     default:
       return state
   }
@@ -18,8 +26,14 @@ const addBlogPost = (dispatch) => {
   }
 }
 
+const deleteBlogPost = (dispatch) => {
+  return (id) => {
+    dispatch({ type: 'delete_blogPost', payload: id })
+  }
+}
+
 export const { Context, Provider } = createDataContext(
   blogReducer,
-  { addBlogPost },
+  { addBlogPost, deleteBlogPost },
   []
 )
